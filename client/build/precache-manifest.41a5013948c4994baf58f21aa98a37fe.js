bf642476c8d2accc5731a064a875a647aa596f55
vadimdoga <vadim.doga1@gmail.com>
2019-12-13 12:51:00 +0200

    new path igonre

---
+++ client/build/precache-manifest.41a5013948c4994baf58f21aa98a37fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe6e3189d495161633c7aa550cffeca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "361625e3aa62d957a31c",
+    "url": "/static/css/main.c8443198.chunk.css"
+  },
+  {
+    "revision": "1ac2433780097d47a8d5",
+    "url": "/static/js/2.e7766433.chunk.js"
+  },
+  {
+    "revision": "361625e3aa62d957a31c",
+    "url": "/static/js/main.f716ac43.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
